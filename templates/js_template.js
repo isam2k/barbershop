aafdd369fb8002858bbafeda8da57b842065dfb6
Samuel Aeberhard <samuel.aeberhard@gmail.com>
2015-11-11 20:45:41 +0100

    Adds template files to the project

---
+++ templates/js_template.js
@@ -0,0 +1,18 @@
+/**
+ * [description]
+ *
+ * @author [author name]
+ * @version [version code]
+ * @copyright [company name] [date]
+ */
+
+/**
+ * [function description]
+ *
+ * @param [param name] {[param type]} [param description]
+ * @return [return description]
+ */
+function example([param name]) {
+    [content]
+    return [something];
+} // example
---
+++ templates/php_template.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * [description of file or class]
+ *
+ * @package [package name]
+ * @author [author name]
+ * @copyright [company name] [date]
+ * @version [version code]
+ */
+
+[content]
+?>
